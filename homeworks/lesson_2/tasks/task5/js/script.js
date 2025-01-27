126f44f82b5b87f77a6ba8eea938122bfdedc4b8
GrishinViacheslav <40839049+GrishinViacheslav@users.noreply.github.com>
2025-01-27 08:37:01 +0200

    Update script.js

--- homeworks/lesson_2/tasks/task5/js/script.js
+++ homeworks/lesson_2/tasks/task5/js/script.js
@@ -1,7 +1,9 @@
 "use script";
 // Задача 5. Дано кількість секунд, що пройшла від початку доби. Визначити скільки це годин і хвилин.
-const seconds = parseInt(prompt("Введіть кількість секунд з початку доби", 0));
+const currentSeconds = parseInt(prompt("Введіть кількість секунд з початку доби", 0));
+const secondsInMinute = 60;
+const secondsInHour = 3600;
 
-const hours = parseInt(seconds / (60 * 60));
-const minutes = parseInt((seconds - hours * 60 * 60) / 60);
+const hours = parseInt(currentSeconds / secondsInHour);
+const minutes = parseInt((currentSeconds - secondsInHour) / secondsInMinute);
 document.write(`<div>З початку доби пройшло ${hours} годин та ${minutes} хвилин</div>`);
